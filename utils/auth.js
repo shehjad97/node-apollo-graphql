f05185cad564931bdd27399a561b23a76e09461c
shehjad97 <shehjadcse@gmail.com>
2023-06-25 14:35:41 +0600

    removed moment.js and updated generateToken func

--- utils/auth.js
+++ utils/auth.js
@@ -1,15 +1,12 @@
 const jwt = require('jsonwebtoken');
-const moment = require("moment");
 
 const generateToken = (user) => {
-    const accessExpiration = moment().add(parseInt(process.env.JWT_ACCESS_EXPIRATION), "days");
     const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_ACCESS_SECRET, {
-        expiresIn: accessExpiration,
+        expiresIn: `${process.env.JWT_ACCESS_EXPIRATION}d`,
     });
 
-    // const refreshExpiration = moment().add(parseInt(process.env.JWT_REFRESH_EXPIRATION), "days");
     // const refreshToken = jwt.sign({ userId: user._id }, process.env.JWT_REFRESH_SECRET, {
-    //     expiresIn: refreshExpiration,
+    //     expiresIn: `${process.env.JWT_REFRESH_EXPIRATION}d`,
     // });
 
     // return { accessToken, refreshToken };
